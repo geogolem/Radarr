87ef952564a2759c7157f1b7ffcfe3bb285b1541
geogolem <geogolem@gmail.com>
2019-11-14 17:56:06 -0500

    port from augmentMovieInfo branch which was based on develop

--- src/NzbDrone.Core/Configuration/IConfigService.cs
+++ src/NzbDrone.Core/Configuration/IConfigService.cs
@@ -69,6 +69,7 @@ namespace NzbDrone.Core.Configuration
         TMDbCountryCode CertificationCountry { get; set; }
 
         //UI
+        string OmdbApiKey { get; set; }
         int FirstDayOfWeek { get; set; }
         string CalendarWeekColumnHeader { get; set; }
         MovieRuntimeFormatType MovieRuntimeFormat { get; set; }
