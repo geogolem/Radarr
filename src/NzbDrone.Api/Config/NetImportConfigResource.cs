146a76fe04717506e381006a21594e06c78e9790
geogolem <geogolem@gmail.com>
2020-09-06 15:56:54 -0400

    incomplete: user specified tag on Lists options Page for cleanLibrary tag

--- src/NzbDrone.Api/Config/NetImportConfigResource.cs
+++ src/NzbDrone.Api/Config/NetImportConfigResource.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NzbDrone.Core.Configuration;
 using Radarr.Http.REST;
 
@@ -8,6 +9,7 @@ namespace NzbDrone.Api.Config
         public int ImportListSyncInterval { get; set; }
         public string ListSyncLevel { get; set; }
         public string ImportExclusions { get; set; }
+        public HashSet<int> CleanLibraryTags { get; set; }
     }
 
     public static class NetImportConfigResourceMapper
@@ -19,6 +21,7 @@ namespace NzbDrone.Api.Config
                 ImportListSyncInterval = model.ImportListSyncInterval,
                 ListSyncLevel = model.ListSyncLevel,
                 ImportExclusions = model.ImportExclusions,
+                CleanLibraryTags = model.CleanLibraryTags
             };
         }
     }
